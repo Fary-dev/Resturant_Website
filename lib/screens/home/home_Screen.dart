1482576f1718fdd3c51bc79edfec2c4b71f8e882
fary <fbakhsheshi@ymail.com>
2021-05-14 01:30:57 +0200

    14.05.2021

--- lib/screens/home/home_Screen.dart
+++ lib/screens/home/home_Screen.dart
@@ -12,40 +12,36 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     final Size size = MediaQuery.of(context).size;
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       body: ListView(
         physics: ScrollPhysics(parent: AlwaysScrollableScrollPhysics()),
         shrinkWrap: true,
         children: [
-          Expanded(
-            child: Container(
-                height: size.height,
-                width: size.width,
-                decoration: BoxDecoration(
-                    gradient: LinearGradient(
-                  begin: Alignment.topCenter,
-                  end: Alignment.bottomCenter,
-                  colors: [
-                    Color(0xFFDB0707),
-                    Color(0xFF02000B),
-                  ],
+          Container(
+            height: size.height,
+            width: size.width,
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+                colors: [
+                  Color(0xFFDB0707),
+                  Color(0xFF02000B),
+                ],
+              ),
+            ),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                CustomAppBar(),
+                Spacer(),
+                Body(),
+                Spacer(
+                  flex: 2,
                 )
-                    // image: DecorationImage(
-                    //   image: AssetImage('assets/WebSiteBkg.jpg'),
-                    //   fit: BoxFit.cover,
-                    // ),
-                    ),
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  crossAxisAlignment: CrossAxisAlignment.end,
-                  children: [
-                    CustomAppBar(),
-                    Spacer(),
-                    Body(),
-                    Spacer(
-                      flex: 2,
-                    )
-                  ],
-                )),
+              ],
+            ),
           ),
         ],
       ),
